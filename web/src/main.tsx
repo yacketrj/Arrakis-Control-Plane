0d5760e0126c5d6a45bee8849c0276bbd1049e40
Ron Yacketta <yacketrj@users.noreply.github.com>
2026-05-22 00:20:21 -0700

    fix: remove unused Clerk provider

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,18 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { ClerkProvider } from '@clerk/react'
-
-const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {publishableKey ? (
-      <ClerkProvider publishableKey={publishableKey} afterSignOutUrl="/">
-        <App />
-      </ClerkProvider>
-    ) : (
-      <App />
-    )}
+    <App />
   </StrictMode>,
 )
