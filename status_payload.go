bc388ab72c03b48dd4ae51c650ce5a0150624da2
Ron Yacketta <yacketrj@users.noreply.github.com>
2026-05-24 21:13:26 -0700

    feat: expose runtime and mutation safety status

--- status_payload.go
+++ status_payload.go
@@ -2,11 +2,13 @@ package main
 
 func buildStatusPayload() map[string]any {
 	return map[string]any{
-		"ssh_connected": globalSSH != nil,
-		"db_connected":  globalDB != nil,
-		"pod_ns":        globalPodNS,
-		"ssh_host":      sshHost,
-		"tunnel_mode":   normalizedTunnelMode(),
-		"tunnels":       tunnelStatus(),
+		"ssh_connected":         globalSSH != nil,
+		"db_connected":          globalDB != nil,
+		"pod_ns":                globalPodNS,
+		"ssh_host":              sshHost,
+		"runtime":               normalizeRuntime(serverRuntime),
+		"tunnel_mode":           normalizedTunnelMode(),
+		"tunnels":               tunnelStatus(),
+		"admin_reason_required": adminReasonEnforcementEnabled(),
 	}
 }
