e762f76d346f449bb5b84cbe5b171c3bd5567873
Ron Yacketta <yacketrj@users.noreply.github.com>
2026-06-01 15:57:37 -0700

    Allow PATCH in CORS middleware

--- server.go
+++ server.go
@@ -16,7 +16,7 @@ func corsMiddleware(next http.Handler) http.Handler {
 				w.Header().Set("Access-Control-Allow-Origin", origin)
 				w.Header().Set("Vary", "Origin")
 			}
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Admin-Token, X-Admin-Reason")
 		}
 		if r.Method == http.MethodOptions {
